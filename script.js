c2da0ceceb87304761ea81012e45f809f0d35f57
tannergarner <garnert.w.14@gmail.com>
2024-09-16 10:31:38 -0600

    Add mobile menu dropdown

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ if (link.href === window.location.href) {
 }
 });
 
+const mobileMenu = document.getElementById('mobileMenu');
+
+mobileMenu.addEventListener('click', function () {
+    document.getElementById('navList').classList.toggle('toggleDisplay');
+})
